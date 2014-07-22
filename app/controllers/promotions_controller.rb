5e61540bc3702c7a5f4ca803e31e9898faa737d2
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-07-22 15:58:10 -0700

    Show list of promotions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,5 +1,6 @@
 class PromotionsController < ApplicationController
   def index
-
+    response = RestClient.get "http://www.webappcamp.ws/api/v0/promotions"
+    @promotions = JSON.parse(response)["data"]
   end
 end
