0747c1768e22064871d17c536ed737e3091fb01d
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-07-27 17:50:42 -0700

    Use helper to show course discounts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  include ActionView::Helpers::NumberHelper
-  
-  def display_course_price(num)
-    number_to_currency(num.to_f/100)
-  end
 end
---
+++ app/helpers/promotions_helper.rb
@@ -0,0 +1,17 @@
+module PromotionsHelper
+  include ActionView::Helpers::NumberHelper
+  
+  def display_course_price(num)
+    number_to_currency(num.to_f / 100)
+  end
+
+  def display_discount(original_price, current_price)
+    percent_discount = "#{calculate_discount(original_price, current_price)}% OFF!!!"
+  end
+
+  private
+
+  def calculate_discount(original_price, current_price)
+    (100 * (original_price - current_price).to_f / original_price).round(1)
+  end
+end
