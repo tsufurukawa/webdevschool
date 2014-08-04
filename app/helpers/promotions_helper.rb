b8cb7303a3e8b1beb31abcf1242a0b40f712230e
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-08-03 17:49:50 -0700

    Display -bash.00 as FREE

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -2,7 +2,7 @@ module PromotionsHelper
   include ActionView::Helpers::NumberHelper
   
   def display_course_price(num)
-    number_to_currency(num.to_f / 100)
+    num.zero? ? "FREE" : number_to_currency(num.to_f / 100)
   end
 
   def display_discount(original_price, current_price)
